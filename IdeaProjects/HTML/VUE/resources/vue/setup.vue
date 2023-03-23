194ba0e08c7d4921cfbd7588548ac34a10cfee28
binjunkai <1176302809@qq.com>
2023-03-23 13:55:04 +0800

    remove duplicate project

---
+++ IdeaProjects/HTML/VUE/resources/vue/setup.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>age : {{age}}</h2>
+  <h2>name : {{name}}</h2>
+  <button @click="changeInfo">Change Info</button>
+</template>
+
+<script>
+  import {ref} from "./vue";
+  export default {
+    name: 'App',
+    setup(){
+      let name = ref('zhangsan')
+      let age = ref(15)
+
+
+      function changeInfo(){
+        name.value = 'lisi'
+        age.value = 23
+      }
+
+      return {
+        name,
+        age,
+        changeInfo
+      }
+    }
+  }
+</script>
